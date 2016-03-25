33ce0617a63b60d67b9339ca096cba5c333c7445
Athman Gude <athmangude@gmail.com>
2016-03-25 14:43:51 +0300

    setting up app with browserify

---
+++ client/components/app/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import {} from './style.less';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Hello world from a React Component</div>
+        );
+    }
+}
+
+export default App;
