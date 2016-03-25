a471ca42eb78096b69a91d55c8dc37cafed4b8e5
Athman Gude <athmangude@gmail.com>
2016-03-25 16:39:59 +0300

    added message-box and message-list modules

---
+++ client/components/message-entry-box/index.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class MessageEntryBox extends Component {
+
+    handleChange(event) {
+        this.props.onChange(event.target.value);
+    }
+
+    handleKeyPress(event) {
+        event.preventDefault();
+
+        if (event.which === 13) {
+            this.props.onSubmit();
+        }
+    }
+
+    render() {
+        return (
+            <div className="message-entry-box">
+                <textarea
+                    name="message"
+                    value={this.props.value}
+                    onChange={this.handleChange.bind(this)}
+                    onKeyPress={this.handleKeyPress.bind(this)} />
+            </div>
+        );
+    }
+}
---
+++ client/components/message-list/index.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <ol className="message-list">
+                {this.props.messages.map((message, index) => {
+                    return (
+                        <li key={`message-${index}`}>
+                            {message.text}
+                        </li>
+                    );
+                })}
+            </ol>
+        );
+    }
+}
+
+export default MessageList;
