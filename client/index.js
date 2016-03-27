ee64757ee4ded0e7f6ddbd4594ee5a16913a36bf
Athman Gude <athmangude@gmail.com>
2016-03-27 22:42:06 +0300

    applying middleware to create store

--- client/index.js
+++ client/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 
-// import App from './components/app'
 import App from 'components/app'
-// import reducers from './reducers';
 import reducers from 'reducers';
 
-const initialState = window.INITIAL_STATE;
-const store = createStore(reducers(initialState));
+import startChat, {chatMiddleWare} from './chat';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, app);
+const initialState = window.INITIAL_STATE;
+const createStoreWithMiddleware = applyMiddleware(chatMiddleWare)(createStore);
+const store = createStoreWithMiddleware(reducers(initialState));
+
+startChat(store);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    app
+);
