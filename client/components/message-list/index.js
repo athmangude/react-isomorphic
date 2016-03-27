3539dc4ddd3e586d88f5bd8c6185d55816f1bbfe
Athman Gude <athmangude@gmail.com>
2016-03-27 23:29:13 +0300

    adding the right message class to response messages, restructured message markup

--- client/components/message-list/index.js
+++ client/components/message-list/index.js
@@ -9,9 +9,12 @@ class MessageList extends Component {
         return (
             <ol className="message-list">
                 {this.props.messages.map((message, index) => {
+                    const messageClass = message.userId !== this.props.userId ? 'is-response' : '';
                     return (
-                        <li key={`message-${index}`}>
-                            {message.text}
+                        <li key={`message-${index}`} className="message-item">
+                            <p className={`message ${messageClass}`}>
+                                {message.text}
+                            </p>
                         </li>
                     );
                 })}
